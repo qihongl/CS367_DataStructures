15d2ce835ee1ce88bc389e2badfc14abda7295e7
LynMQY <mqy0712@126.com>
2015-03-05 19:50:25 -0600

    Update AmazonStore.java

--- CS367_Program2/AmazonStore.java
+++ CS367_Program2/AmazonStore.java
@@ -2,8 +2,8 @@
 //                   ALL STUDENTS COMPLETE THESE SECTIONS
 // Title:            Amazon Store
 // Files:            AmazonStore.java, User.java, DLinkedList.java, 
-//						Product.java, InsufficientCreditException.java, 
-// 						Listnode.java, ListADT.java  
+//			Product.java, InsufficientCreditException.java, 
+// 			Listnode.java, ListADT.java  
 // Semester:         CS367 Spring 2015
 //
 // Author:           Qihong Lu
@@ -43,7 +43,8 @@ public class AmazonStore {
 	//main method
 	public static void main(String args[]) {
 
-		//Populate the two lists using the input files: Products.txt User1.txt User2.txt ... UserN.txt
+		//Populate the two lists using the input files: 
+		// Products.txt User1.txt User2.txt ... UserN.txt
 		if (args.length < 2) {
 			System.out.println("Usage: java AmazonStore [PRODUCT_FILE] [USER1_FILE] [USER2_FILE] ...");
 			System.exit(0);
