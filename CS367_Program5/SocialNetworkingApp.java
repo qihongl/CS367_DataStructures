320c588a993ccf3cd5cb97bfbadd4673be82615d
Qihong Lu <Qihong@dyn-72-33-214-166.uwnet.wisc.edu>
2015-04-24 16:44:44 -0500

    some updates

--- CS367_Program5/SocialNetworkingApp.java
+++ CS367_Program5/SocialNetworkingApp.java
@@ -1,159 +1,179 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-
 public class SocialNetworkingApp {
 
-    /**
-     * Returns a social network as defined in the file 'filename'.
-     * See assignment handout on the expected file format.
-     * @param filename filename of file containing social connection data
-     * @return
-     * @throws FileNotFoundException if file does not exist
-     */
-    public static SocialGraph loadConnections(String filename) throws FileNotFoundException {
-        //TODO
-        return new SocialGraph();
-    }
+	static Scanner stdin = new Scanner(System.in);
+	static SocialGraph graph;
+	static String prompt = ">> ";  // Command prompt
+	
+	private static Scanner scnr; // TODO
 
-    static Scanner stdin = new Scanner(System.in);
-    static SocialGraph graph;
-    static String prompt = ">> ";  // Command prompt
+	/**
+	 * Returns a social network as defined in the file 'filename'.
+	 * See assignment handout on the expected file format.
+	 * @param filename filename of file containing social connection data
+	 * @return
+	 * @throws FileNotFoundException if file does not exist
+	 */
+	public static SocialGraph loadConnections(String filename) throws FileNotFoundException {
+		//TODO
+		if(filename == null) throw new IllegalArgumentException();
+		File inputFile = new File(filename);
+		
+		scnr = new Scanner(inputFile);
+		while(scnr.hasNext()){
+			String userName = scnr.next();
+			System.out.print("*" + userName + ":");
+			if(scnr.hasNext()){ 
+				String friends = scnr.nextLine();
+				System.out.println("\t" + friends);
+			} else {// other wise no friend, still print a line 
+				System.out.println();
+			}
+		} // end of while 
+		
+		
+		scnr.close(); // close the scanner for reading initial input file 
+		return new SocialGraph();
+	}
 
-    /**
-     * Access main menu options to login or exit the application.
-     * 
-     * THIS METHOD HAS BEEN IMPLEMENTED FOR YOU.
-     */
-    public static void enterMainMenu() {
-        boolean exit = false;
-        while (!exit) {
-            System.out.print(prompt);
-            String[] tokens = stdin.nextLine().split(" ");
-            String cmd = tokens[0];
-            String person = (tokens.length > 1 ? tokens[1] : null);
 
-            switch(cmd) {
-                case "login":
-                    System.out.println("Logged in as " + person);
-                    enterSubMenu(person);
-                    System.out.println("Logged out");
-                    break;
-                case "exit":
-                    exit = true;
-                    break;
-                default:
-                    System.out.println("Invalid command");
-            }
-        }
-    }
+	/**
+	 * Access main menu options to login or exit the application.
+	 * 
+	 * THIS METHOD HAS BEEN IMPLEMENTED FOR YOU.
+	 */
+	public static void enterMainMenu() {
+		boolean exit = false;
+		while (!exit) {
+			System.out.print(prompt);
+			String[] tokens = stdin.nextLine().split(" ");
+			String cmd = tokens[0];
+			String person = (tokens.length > 1 ? tokens[1] : null);
 
-    /**
-     * Access submenu options to view the social network from the perspective
-     * of currUser. Assumes currUser exists in the network.
-     * @param currUser
-     */
-    public static void enterSubMenu(String currUser) {
+			switch(cmd) {
+			case "login":
+				System.out.println("Logged in as " + person);
+				enterSubMenu(person);
+				System.out.println("Logged out");
+				break;
+			case "exit":
+				exit = true;
+				break;
+			default:
+				System.out.println("Invalid command");
+			}
+		}
+	}
 
-        // Define the set of commands that have no arguments or one argument
-        String commands =
-                "friends fof logout print\n" +
-                "connection friend unfriend";
-        Set<String> noArgCmds = new HashSet<String>
-                (Arrays.asList(commands.split("\n")[0].split(" ")));
-        Set<String> oneArgCmds = new HashSet<String>
-                (Arrays.asList(commands.split("\n")[1].split(" ")));
+	/**
+	 * Access submenu options to view the social network from the perspective
+	 * of currUser. Assumes currUser exists in the network.
+	 * @param currUser
+	 */
+	public static void enterSubMenu(String currUser) {
 
-        boolean logout = false;
-        while (!logout) {
-            System.out.print(prompt);
+		// Define the set of commands that have no arguments or one argument
+		String commands =
+				"friends fof logout print\n" +
+						"connection friend unfriend";
+		Set<String> noArgCmds = new HashSet<String>
+		(Arrays.asList(commands.split("\n")[0].split(" ")));
+		Set<String> oneArgCmds = new HashSet<String>
+		(Arrays.asList(commands.split("\n")[1].split(" ")));
 
-            // Read user commands
-            String[] tokens = stdin.nextLine().split(" ");
-            String cmd = tokens[0];
-            String otherPerson = (tokens.length > 1 ? tokens[1] : null);
+		boolean logout = false;
+		while (!logout) {
+			System.out.print(prompt);
 
-            // Reject erroneous commands
-            // You are free to do additional error checking of user input, but
-            // this isn't necessary to receive a full grade.
-            if (tokens.length == 0) continue;
-            if (!noArgCmds.contains(cmd) && !oneArgCmds.contains(cmd)) {
-                System.out.println("Invalid command");
-                continue;
-            }
-            if (oneArgCmds.contains(cmd) && otherPerson == null) {
-                System.out.println("Did not specify person");
-                continue;
-            }
+			// Read user commands
+			String[] tokens = stdin.nextLine().split(" ");
+			String cmd = tokens[0];
+			String otherPerson = (tokens.length > 1 ? tokens[1] : null);
 
-            switch(cmd) {
+			// Reject erroneous commands
+			// You are free to do additional error checking of user input, but
+			// this isn't necessary to receive a full grade.
+			if (tokens.length == 0) continue;
+			if (!noArgCmds.contains(cmd) && !oneArgCmds.contains(cmd)) {
+				System.out.println("Invalid command");
+				continue;
+			}
+			if (oneArgCmds.contains(cmd) && otherPerson == null) {
+				System.out.println("Did not specify person");
+				continue;
+			}
 
-            case "connection": {
-                //TODO
-                break;
-            }
+			switch(cmd) {
 
-            case "friends": {
-                //TODO
-                break;
-            }
+			case "connection": {
+				//TODO
+				break;
+			}
 
-            case "fof": {
-                //TODO
-                break;
-            }
+			case "friends": {
+				//TODO
+				break;
+			}
 
-            case "friend": {
-                //TODO
-                break;
-            }
+			case "fof": {
+				//TODO
+				break;
+			}
 
-            case "unfriend": {
-                //TODO
-                break;
-            }
+			case "friend": {
+				//TODO
+				break;
+			}
 
-            case "print": {
-                // This command is left here for your debugging needs.
-                // You may want to call graph.toString() or graph.pprint() here
-            	// You are free to modify this or remove this command entirely.
-            	//
-                // YOU DO NOT NEED TO COMPLETE THIS COMMAND
-                // THIS COMMAND WILL NOT BE PART OF GRADING
-                break;
-            }
+			case "unfriend": {
+				//TODO
+				break;
+			}
 
-            case "logout":
-                logout = true;
-                break;
-            }  // End switch
-        }
-    }
+			case "print": {
+				// This command is left here for your debugging needs.
+				// You may want to call graph.toString() or graph.pprint() here
+				// You are free to modify this or remove this command entirely.
+				//
+				// YOU DO NOT NEED TO COMPLETE THIS COMMAND
+				// THIS COMMAND WILL NOT BE PART OF GRADING
+				break;
+			}
 
-    /**
-     * Commandline interface for a social networking application.
-     *
-     * THIS METHOD HAS BEEN IMPLEMENTED FOR YOU.
-     *
-     * @param args
-     */
-    public static void main(String[] args) {
+			case "logout":
+				logout = true;
+				break;
+			}  // End switch
+		}
+	}
 
-        if (args.length != 1) {
-            System.out.println("Usage: java SocialNetworkingApp <filename>");
-            return;
-        }
-        try {
-            graph = loadConnections(args[0]);
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found");
-        }
+	/**
+	 * Commandline interface for a social networking application.
+	 *
+	 * THIS METHOD HAS BEEN IMPLEMENTED FOR YOU.
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
 
-        enterMainMenu();
+		if (args.length != 1) {
+			System.out.println("Usage: java SocialNetworkingApp <filename>");
+			return;
+		}
+		try {
+			graph = loadConnections(args[0]);
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		}
 
-    }
+		enterMainMenu();
+
+	}
 
 }
